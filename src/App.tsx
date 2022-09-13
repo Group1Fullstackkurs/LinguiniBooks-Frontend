6106319da99a2b70fb8e7bd2fef15228f41eba6d
jt1230 <juliatang95@gmail.com>
2022-09-13 10:37:09 +0200

    hämtar från localhost

--- src/App.tsx
+++ src/App.tsx
@@ -5,38 +5,24 @@ import Landingpage from './components/Landingpage'
 
 function App() {
   const [bookData, setBookData] = useState([])
-	const [book, setBook] = useState([])
+	const [books, setBooks] = useState([])
 	const [pokeList, setPokeList] = useState([])
 	//const url = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=1000'
-  const url = 'https://linguinibooksapi20220912102408.azurewebsites.net/api/Book'
-  //const url = 'https://linguinibooksapi20220912102408.azurewebsites.net/WeatherForecast'
+  	//const url = 'https://linguinibooksapi20220912102408.azurewebsites.net/api/Book'
+  	//const url = 'https://linguinibooksapi20220912102408.azurewebsites.net/WeatherForecast'
+	const url = 'https://localhost:7269/api/Book'
 
   useEffect(() => { 
 		const getBooks = async() => {
 			const response = await fetch(url) 
 			let data = await response.json()
-      console.log(data)
+			console.log('hämtar data...')
+			console.log(data)
 			setBookData(data.results)
 		}
 		getBooks()
 
 	}, [])
-
-	// useEffect(() => {
-	// 	if(bookData == null)
-	// 		return
-	// 	else{
-	// 		const getBooks = async(response: any[]) => {		
-	// 			response.map(async(book) =>{
-	// 				const result = await fetch(book.url)
-	// 				let data = await result.json()
-	// 				setBook(data)
-	// 			})
-	// 		}
-	// 		getBooks(bookData)
-	// 	}
-
-	// }, [bookData])
 
   return (
     <div className="App">
