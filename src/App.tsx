e3d8367db4a30eef28a9795273447957b93737f8
Mikael Hargeus <Mikael.Hargeus@live.se>
2022-09-14 11:54:36 +0200

    Fixed API connection and showing data in console.
    Co-authored-by: Julia <jt1230@users.noreply.github.com>
    Co-authored-by: Phoenixbird7 <Phoenixbird7@users.noreply.github.com>
    Co-authored-by: Camilla Gustafsson <CGDM101@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,11 @@ import Landingpage from './components/Landingpage'
 
 function App() {
   const [bookData, setBookData] = useState([])
-	const [books, setBooks] = useState([])
-	const [pokeList, setPokeList] = useState([])
+  const [bookList, setBookList] = useState([])
 	//const url = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=1000'
   	//const url = 'https://linguinibooksapi20220912102408.azurewebsites.net/api/Book'
   	//const url = 'https://linguinibooksapi20220912102408.azurewebsites.net/WeatherForecast'
 	const url = 'https://localhost:7269/api/Book'
-
-  useEffect(() => { 
-		const getBooks = async() => {
-			const response = await fetch(url) 
-			let data = await response.json()
-			console.log('hämtar data...')
-			console.log(data)
-			setBookData(data.results)
-		}
-		getBooks()
-
-	}, [])
 
   return (
     <div className="App">
