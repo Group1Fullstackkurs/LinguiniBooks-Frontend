387e746f6fe7294c436514286b3683d7ca6a9021
jt1230 <juliatang95@gmail.com>
2022-09-14 15:29:03 +0200

    Går att displaya en rad med nya, en rad med begagnade, en rad med bägge
    Co-authored-by: mhargeus

--- src/components/Landingpage.tsx
+++ src/components/Landingpage.tsx
@@ -13,28 +13,78 @@ function Landingpage() {
         fetchAllBooks().then(books =>setBookList(books)
         )
     }, [])
-    
-    //const book2: BookModel = new BookModel()
-    
+        
     
     return (
-        <div className="LandingPage">
-            
-            <div className="bookbox">
-                {bookList.map(book => 
-                    {return(
-                        <div className='book-info' key={book.id}>
-                            <p>Title: {book.title}</p>
-                            <p>Author: {book.firstName} {book.lastName}</p>
-                            <p>Publication Year: {book.publicationYear}</p>
-                            <p>Category: {book.category}</p>
-                            <p>Condition: {(book.new) ? 'New' : 'Used'}</p>
-                            <p>Seller: {book.seller}</p>
-                        </div>
-                    )})
-                }
-            </div>
+      <div className="LandingPage">
+        <h2>Populära böcker i nytt skick</h2>
+        <div className="bookbox">
+          {bookList
+            .filter((book) => {
+              if (book.new == true) {
+                return book;
+              }
+            })
+            .map((book) => {
+              return (
+                <div className="book-info" key={book.id}>
+                  <p>Title: {book.title}</p>
+                  <p>
+                    Author: {book.firstName} {book.lastName}
+                  </p>
+                  <p>Publication Year: {book.publicationYear}</p>
+                  <p>Category: {book.category}</p>
+                  <p>Condition: {book.new ? "New" : "Used"}</p>
+                  <p>Seller: {book.seller}</p>
+                </div>
+              );
+            })
+            .slice(1, 6)}
         </div>
-    )
+        <h2>Populära böcker i begagnat skick</h2>
+        <div className="bookbox">
+          {bookList
+            .filter((book) => {
+              if (book.new == false) {
+                return book;
+              }
+            })
+            .map((book) => {
+              return (
+                <div className="book-info" key={book.id}>
+                  <p>Title: {book.title}</p>
+                  <p>
+                    Author: {book.firstName} {book.lastName}
+                  </p>
+                  <p>Publication Year: {book.publicationYear}</p>
+                  <p>Category: {book.category}</p>
+                  <p>Condition: {book.new ? "New" : "Used"}</p>
+                  <p>Seller: {book.seller}</p>
+                </div>
+              );
+            })
+            .slice(1, 6)}
+        </div>
+        <h2>Populära böcker i nytt eller begagnat skick</h2>
+        <div className="bookbox">
+          {bookList
+            .map((book) => {
+              return (
+                <div className="book-info" key={book.id}>
+                  <p>Title: {book.title}</p>
+                  <p>
+                    Author: {book.firstName} {book.lastName}
+                  </p>
+                  <p>Publication Year: {book.publicationYear}</p>
+                  <p>Category: {book.category}</p>
+                  <p>Condition: {book.new ? "New" : "Used"}</p>
+                  <p>Seller: {book.seller}</p>
+                </div>
+              );
+            })
+            .slice(1, 6)}
+        </div>
+      </div>
+    );
 }
 export default Landingpage
