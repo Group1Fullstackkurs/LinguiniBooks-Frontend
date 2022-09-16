0169ca42d5c812d7f2b7158c532ed8a35b2d3ffd
jt1230 <juliatang95@gmail.com>
2022-09-16 16:17:39 +0200

    Går att söka efter boktitelar
    Co-authored-by: mhargeus <mhargeus@users.noreply.github.com>
    Co-authored-by: Leon Larsson <Leon-Hobby@users.noreply.github.com>

---
+++ src/components/Profilepage.tsx
@@ -0,0 +1,16 @@
+import "./Profilepage.css";
+import { useState, useEffect } from "react";
+
+function Profilepage() {
+ 
+
+  return (
+      <section>
+      <div className="ProfilePage">
+        <h3>This is a nice profile page, good design.</h3>     
+      </div>
+
+    </section>
+  )
+}
+export default Profilepage;
