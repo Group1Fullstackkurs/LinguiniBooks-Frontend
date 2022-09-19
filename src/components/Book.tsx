c272742813b0b06bc7273878860c2a065f35d5a0
Julia <juliatang95@gmail.com>
2022-09-19 12:49:07 +0200

    new/used icon on books

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -3,18 +3,32 @@ import { BookModel } from "../Typescript/BookModel";
 
 function Book(book: BookModel) {
   return (
-    <div className="book">
-      <div className="book-title">
-        <p>{book.title}</p>
+    <div className="book-container">
+      <div className="book">
+        {(book.new) ? <div className="book-new-icon">
+          New
+        </div> : <div className="book-new-icon">
+          Used
+        </div>}
+        <div className="book-title">
+          <p>{book.title}</p>
+        </div>
+        <div className="book-author">
+          <p>{book.firstName} {book.lastName}</p>
+        </div>
       </div>
-      <div className="book-author">
-        <p>{book.firstName} {book.lastName}</p>
+      <div className="book-sale-info">
+        <p>{book.price}</p>
       </div>
-      
-      {/* <p>Publication Year: {book.publicationYear}</p>
-      <p>Category: {book.category}</p>
-      <p>Condition: {book.new ? "New" : "Used"}</p>
-      <p>Seller: {book.seller}</p> */}
+      <a href="http://google.com" target="_blank">
+        <div className="book-cart-btn">
+          Add to cart - ska länka till cart
+        </div>
+      </a>
+        {/* <p>Publication Year: {book.publicationYear}</p>
+        <p>Category: {book.category}</p>
+        <p>Condition: {book.new ? "New" : "Used"}</p>
+        <p>Seller: {book.seller}</p> */}
     </div>
   );
 }
