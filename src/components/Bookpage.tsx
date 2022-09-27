3afa2157b92e758a9823a9b914d494bf789edd89
jt1230 <juliatang95@gmail.com>
2022-09-27 13:06:11 +0200

    Category, AZ, author, year toggle/filtering works

--- src/components/Bookpage.tsx
+++ src/components/Bookpage.tsx
@@ -1,33 +1,23 @@
-import "./Bookpage.css";
-import { BookModel } from "../Typescript/BookModel";
-import { useState, useEffect } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 import pureBooksState from "../atoms/pureBooksState";
-import filteredBooksState from '../atoms/filteredBooksState'
-import searchInfoState from "../atoms/searchInfoState";
+import filteredBooksState from "../atoms/filteredBooksState";
 import Book from "./Book";
-import filter from "../Typescript/Filter";
-import { FilterType } from "../Typescript/EnumFilterType";
+import "./Bookpage.css";
 
 function Bookpage() {
-  const [bookList, setBookList] = useState(Array<BookModel>);
-  const [infoState, setInfoState] = useRecoilState(searchInfoState);
   const [filteredBooks, setFilteredBooks] = useRecoilState(filteredBooksState);
   const pureBooks = useRecoilValue(pureBooksState);
-
-  useEffect(() => {}, [filteredBooks]);
 
   return (
     <div className="book-page">
       <div className="book-search-box">
-        {filteredBooks
-          .map((book) => {
-            return (
-              <div key={book.id} className="book-search">
-                <Book {...book} />
-              </div>
-            );
-          })}
+        {filteredBooks.map((book) => {
+          return (
+            <div key={book.id} className="book-search">
+              <Book {...book} />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
